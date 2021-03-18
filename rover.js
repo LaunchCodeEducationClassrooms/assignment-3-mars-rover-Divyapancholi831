5383454eb5d8e269133a32ca28eec63c09cbfa9a
Divyapancholi <DIVYAPANCHOLI1995@GMAIL.COM>
2021-03-18 19:58:29 +0000

    made changes in message,js file

--- rover.js
+++ rover.js
@@ -1,5 +1,15 @@
 class Rover {
    // Write code here!
+   Constructor(position)
+   {
+     this.position=position;
+     this.mode='NORMAL';
+     this.generatorWatts=110;
+   }
+   /*receiveMessage(message)
+   {
+     return {};
+   }*/
 }
 
 module.exports = Rover;
